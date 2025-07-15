158d2801f2316748590ed7818bb977a7b9b47ad3
devilhazretleri <emoamas2ci@gmail.com>
2025-07-15 04:41:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "6741595016:AAFc-J1kdqIBnpYlJFUAhGnPFL0lxQqJPMQ"
+TOKEN = "6741595016:AAFhsSuLf97f8bsDMUm211iHfEvPdJLnRQ0"
 KUFUR_KELIMELERI = ["salak", "aptal", "küfür1", "küfür2"]
 GIRIS_MESAJI = "👋 Hoş geldin, {name}! Kurallara uymayı unutma."
