962faa7cb0716f2a43adb4d329d616bf1625f4bd
devilhazretleri <emoamas2ci@gmail.com>
2025-07-15 04:10:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "BOT_TOKENİNİ_BURAYA_YAZ"
+TOKEN = "6741595016:AAFc-J1kdqIBnpYlJFUAhGnPFL0lxQqJPMQ"
 KUFUR_KELIMELERI = ["salak", "aptal", "küfür1", "küfür2"]
 GIRIS_MESAJI = "👋 Hoş geldin, {name}! Kurallara uymayı unutma."
